407b6b7020cf5f50cfeaf6eb35c38c21bfd3f4e7
Arman Simonyan <ithacks01@gmail.com>
2024-06-13 20:29:04 +0400

    Entity_Fraemwork

--- MovieWebApp/obj/Debug/net8.0/MovieWebApp.AssemblyInfo.cs
+++ MovieWebApp/obj/Debug/net8.0/MovieWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e725ddc715b34bd52b2cfde43cc62e5c78bd0516")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e68b3c58e6bbfb5ca223e758a1db89eb4a28e87")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
